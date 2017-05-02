d90df232e4b54a8edbb3b0c2113f3e7dd15fc226
LeidyRosmery <rosm3rita@hotmail.com>
2017-05-02 18:17:02 -0500

    funciona scroll

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
 var mostrar=document.getElementById("mostrar");
 var inputs=document.getElementsByClassName("oculto");
 var tamaño=document.getElementsByClassName("turn");
+var menu=document.getElementsByClassName("inicio");
+var letras=document.getElementsByClassName("letra");
 mostrar.addEventListener("click",function(){
     tamaño[0].style.height="556px";
   inputs[0].style.display="block";
@@ -8,12 +10,25 @@ mostrar.addEventListener("click",function(){
       inputs[2].style.display="block";
 
 });
+
 var lastScrollTop = 0;
 window.addEventListener("scroll", function() {
     var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
     if (currentScroll > lastScrollTop) {
-        
+       menu[0].style.background= "#fff";
+      letras[0].style.color= "#000";
+      letras[1].style.color= "#000";
+      letras[2].style.color= "#000";
+      letras[3].style.color= "#000";
     }
-  }
+    else{
+      if( currentScroll <= 3 ){
+        menu[0].style.background= "#000";
+        letras[0].style.color= "#fff";
+        letras[1].style.color= "#fff";
+        letras[2].style.color= "#fff";
+        letras[3].style.color= "#fff";
+      }
+    }
     lastScrollTop = currentScroll;
 }, false);
