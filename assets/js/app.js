82aabd4806066c72644a2d39bfe4ee42485127a3
LeidyRosmery <rosm3rita@hotmail.com>
2017-05-02 17:54:08 -0500

    scroll

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,12 @@ mostrar.addEventListener("click",function(){
       inputs[2].style.display="block";
 
 });
+var lastScrollTop = 0;
+window.addEventListener("scroll", function() {
+    var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    if (currentScroll > lastScrollTop) {
+        
+    }
+  }
+    lastScrollTop = currentScroll;
+}, false);
